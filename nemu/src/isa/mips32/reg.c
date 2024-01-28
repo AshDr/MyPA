b44c242dd0fce86d1f05fddb76dcea67c1be1823
tracer-ics2023 <tracer@njuics.org>
2024-01-28 20:29:33 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:29:33 up 1 day, 12:10,  3 users,  load average: 0.12, 0.29, 0.26

--- nemu/src/isa/mips32/reg.c
+++ nemu/src/isa/mips32/reg.c
@@ -26,9 +26,9 @@ const char *regs[] = {
 
 void isa_reg_display() {
   printf("goin\n");
-  for(int i = 0; i < 32; i++) {
-    printf("%s %x %u\n", reg_name(i), gpr(i), gpr(i));
-  }
+  // for(int i = 0; i < 32; i++) {
+  //   printf("%s %x %u\n", reg_name(i), gpr(i), gpr(i));
+  // }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
