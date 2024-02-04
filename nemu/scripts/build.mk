2373641e940e0c247e7ece3abb8e13cf6b6d99cc
tracer-ics2023 <tracer@njuics.org>
2024-02-04 15:45:21 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:45:21 up 2 days,  7:21,  3 users,  load average: 0.13, 0.07, 0.01

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -32,7 +32,7 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
-	@$(CC) $(CFLAGS) $(SO) -E -MF /dev/null $< | \
+	@$(CC) $(CFLAGS) $(SO) -E $< | \
 		grep -ve '^#' | \
 		clang-format - > $(basename $@).i
 	$(call call_fixdep, $(@:.o=.d), $@)
