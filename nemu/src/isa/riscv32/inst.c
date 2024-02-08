fcd80e7f57913d6d56ac2d7cdebd409fcb114eaf
tracer-ics2023 <tracer@njuics.org>
2024-02-08 18:34:59 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:34:59 up 4 days, 11:21,  4 users,  load average: 0.96, 0.39, 0.16

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -65,7 +65,11 @@ static int decode_exec(Decode *s) {
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
-
+  // printf("%u!", INSTPAT_INST(s));
+  for(int i = sizeof(uint) * 8 - 1; i >= 0; --i) {
+      printf("%d", (INSTPAT_INST(s) >> i) & 1); // 逐位打印
+  }
+  printf("!!!\n");
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
