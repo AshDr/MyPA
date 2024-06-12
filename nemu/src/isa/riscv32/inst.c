b99236fd968011310dd6fb5925ec1f894584e5b9
tracer-ics2023 <tracer@njuics.org>
2024-06-13 01:03:47 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:03:47 up 13:02,  4 users,  load average: 0.05, 0.14, 0.18

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -55,6 +55,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immS(); break;
+    case TYPE_R: src1R(); src2R(); break;
   }
   Log("src1: %u, src2: %u, imm:%u", *src1, *src2, *imm);
 }
