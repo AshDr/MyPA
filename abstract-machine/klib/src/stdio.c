2bff54ddf271c0c7f0e42d496629ddc301f1421b
tracer-ics2023 <tracer@njuics.org>
2024-06-23 00:15:49 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:15:49 up 2 days, 20:18,  1 user,  load average: 0.15, 0.43, 0.36

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,66 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+int int_to_str(int val, char *str) {
+  int idx = 0, is_neg = 0;
+  if(val == 0) {
+    str[idx++] = '0';
+    str[idx] = '\0';
+    return 1;
+  }
+  if(val < 0) {
+    is_neg = 1;
+    val = -val;
+  }
+  while(val > 0) {
+    str[idx++] = val % 10 + '0';
+    val /= 10;
+  }
+  if(is_neg) {
+    str[idx++] = '-';
+  }
+  str[idx] = '\0';
+  int l = 0, r = idx - 1;
+  while(l < r) {
+    char tmp = str[l];
+    str[l] = str[r];
+    str[r] = tmp;
+    l++;
+    r--;
+  }
+  return idx;
+}
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  const char *p = fmt;
+  char buf[50];
+  while(*p != '\0') {
+    if(*p == '%') {
+      ++p;
+      switch (*p) {
+        case 's': {
+          char *s = va_arg(ap, char *);
+          while(*s != '\0') {
+            *out++ = *s++;
+          }
+          break;
+        }
+        case 'd': {
+          int val = va_arg(ap, int);
+          int len = int_to_str(val, buf);
+          if(len >= 50) panic("length of int > 50");
+          strcpy(out, buf);
+          out += len;
+          break;
+        }
+      }
+    }else {
+      *out++ = *p;
+    }
+  }
+  va_end(ap);
+  return 0; // 如果成功，则返回写入的字符总数，不包括字符串追加在字符串末尾的空字符。如果失败，则返回一个负数
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
