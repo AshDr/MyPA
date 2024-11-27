06860492308f6393ac484595d2b350027be09645
tracer-ics2023 <tracer@njuics.org>
2024-11-27 22:54:00 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:54:00 up 40 min,  1 user,  load average: 0.03, 0.07, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,39 +1,30 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
 int int_to_str(int val, char *str) {
   int idx = 0, is_neg = 0;
-  if(val == 0) {
+  if (val == 0) {
     str[idx++] = '0';
     str[idx] = '\0';
     return 1;
   }
-  if(val < 0) {
+  if (val < 0) {
     is_neg = 1;
     val = -val;
   }
-  while(val > 0) {
+  while (val > 0) {
     str[idx++] = val % 10 + '0';
     val /= 10;
   }
-  if(is_neg) {
+  if (is_neg) {
     str[idx++] = '-';
   }
   str[idx] = '\0';
   int l = 0, r = idx - 1;
-  while(l < r) {
+  while (l < r) {
     char tmp = str[l];
     str[l] = str[r];
     str[r] = tmp;
@@ -42,39 +33,90 @@ int int_to_str(int val, char *str) {
   }
   return idx;
 }
-int sprintf(char *out, const char *fmt, ...) {
+
+int printf(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   const char *p = fmt;
   char buf[50];
-  while(*p != '\0') {
-    if(*p == '%') {
+  int f = 1;
+  int length = 0;
+  while (*p != '\0') {
+    if (*p == '%') {
       ++p;
       switch (*p) {
-        case 's': {
-          char *s = va_arg(ap, char *);
-          while(*s != '\0') {
-            *out++ = *s++;
-          }
-          break;
+      case 's': {
+        char *s = va_arg(ap, char *);
+        while (*s != '\0') {
+          putch(*s++);
+          ++length;
         }
-        case 'd': {
-          int val = va_arg(ap, int);
-          int len = int_to_str(val, buf);
-          if(len >= 50) panic("length of int > 50");
-          strcpy(out, buf);
-          out += len;
-          break;
-        }
+        break;
       }
-    }else {
+      case 'd': {
+        int val = va_arg(ap, int);
+        int len = int_to_str(val, buf);
+        if (len >= 50)
+          panic("length of int > 50");
+        for (int i = 0; i < len; ++i) {
+          putch(buf[i]);
+        }
+        length += len;
+        break;
+      }
+      default:
+        f = 0;
+      }
+    } else {
+      putch(*p);
+      ++length;
+    }
+    ++p;
+  }
+  va_end(ap);
+  if (f == 0)
+    return 0;
+  return length;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  panic("Not implemented");
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  const char *p = fmt;
+  char *start = out;
+  char buf[50];
+  while (*p != '\0') {
+    if (*p == '%') {
+      ++p;
+      switch (*p) {
+      case 's': {
+        char *s = va_arg(ap, char *);
+        strcpy(out, s);
+        out += strlen(out);
+        break;
+      }
+      case 'd': {
+        int val = va_arg(ap, int);
+        int len = int_to_str(val, buf);
+        if (len >= 50)
+          panic("length of int > 50");
+        strcpy(out, buf);
+        out += len;
+        break;
+      }
+      }
+    } else {
       *out++ = *p;
     }
     ++p;
   }
   *out = '\0';
   va_end(ap);
-  return strlen(fmt); // 如果成功，则返回写入的字符总数，不包括字符串追加在字符串末尾的空字符。如果失败，则返回一个负数
+  return out - start;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
