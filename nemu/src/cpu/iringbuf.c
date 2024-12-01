4772192421b5721e1ceb6b0ff667bcb567d80427
tracer-ics2023 <tracer@njuics.org>
2024-12-01 16:35:49 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:35:49 up 39 min,  3 users,  load average: 0.63, 0.29, 0.45

--- nemu/src/cpu/iringbuf.c
+++ nemu/src/cpu/iringbuf.c
@@ -1,22 +1,21 @@
 #include <cpu/iringbuf.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 struct IRingBuf ringbuf;
 void init_IRingBuf() {
-    ringbuf.size = 1024;
-    ringbuf.p = 0;
-    ringbuf.buf = malloc(ringbuf.size);
+  ringbuf.size = 1024;
+  ringbuf.p = 0;
+  ringbuf.buf = malloc(ringbuf.size);
 }
 void write_IRingBuf(const char *str) {
-    if(strlen(str) + ringbuf.p < ringbuf.size) {
-        ringbuf.p += snprintf(ringbuf.buf + ringbuf.p, ringbuf.size - ringbuf.p, "%s\n", str);
-    }
-    else {
-        ringbuf.p = 0;
-        ringbuf.p += snprintf(ringbuf.buf + ringbuf.p, ringbuf.size - ringbuf.p, "%s\n", str);
-    }
+  if (strlen(str) + ringbuf.p < ringbuf.size) {
+    ringbuf.p += snprintf(ringbuf.buf + ringbuf.p, ringbuf.size - ringbuf.p,
+                          "%s\n", str);
+  } else {
+    ringbuf.p = 0;
+    ringbuf.p += snprintf(ringbuf.buf + ringbuf.p, ringbuf.size - ringbuf.p,
+                          "%s\n", str);
+  }
 }
-void print_IRingBuf() {
-    printf("%s\n", ringbuf.buf);
-}
+void print_IRingBuf() { printf("%s\n", ringbuf.buf); }
