8019dc0627a854f01efa2c7865c8cea4e800e0a2
tracer-ics2023 <tracer@njuics.org>
2024-12-02 23:28:13 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:28:13 up  2:51,  4 users,  load average: 0.09, 0.28, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,14 +4,26 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t res_data = inl(VGACTL_ADDR);
+  int w = res_data >> 16;
+  int h = res_data & 0xffff; // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++)
+    fb[i] = i;
+  // init vmem data, single pixel is 4 bytes
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  uint32_t data = inl(VGACTL_ADDR);
+  uint32_t width = data >> 16;
+  uint32_t height = data & 0xffff;
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = width,
+                           .height = height,
+                           .vmemsz = width * height * 4};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
@@ -20,6 +32,4 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
