9647ed12f5ffd2e6101d5b862601f2fc988918d4
tracer-ics2023 <tracer@njuics.org>
2025-01-14 16:15:41 +0800

    >  compile NEMU 221220000 张三 Linux ashdr 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  16:15:41 up 125 days, 23:34,  1 user,  load average: 0.77, 0.69, 0.66

--- nemu/include/utils.h
+++ nemu/include/utils.h
@@ -56,26 +56,28 @@ uint64_t get_time();
 
 #define ANSI_FMT(str, fmt) fmt str ANSI_NONE
 
-#define log_write(...)                                                         \
-  IFDEF(                                                                       \
-      CONFIG_TARGET_NATIVE_ELF, do {                                           \
-        extern FILE *log_fp;                                                   \
-        extern bool log_enable();                                              \
-        if (log_enable()) {                                                    \
-          fprintf(log_fp, __VA_ARGS__);                                        \
-          fflush(log_fp);                                                      \
-        }                                                                      \
-      } while (0))
+#define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
+  do { \
+    extern FILE* log_fp; \
+    extern bool log_enable(); \
+    if (log_enable()) { \
+      fprintf(log_fp, __VA_ARGS__); \
+      fflush(log_fp); \
+    } \
+  } while (0) \
+)
 
-#define _Log(...)                                                              \
-  do {                                                                         \
-    printf(__VA_ARGS__);                                                       \
-    log_write(__VA_ARGS__);                                                    \
-  } while (0)
+#define _Log(...) \
+  do { \
+    printf(__VA_ARGS__); \
+    log_write(__VA_ARGS__); \
+ } while (0)
+
 
 void trace_func_call(paddr_t pc, paddr_t target);
 
 void trace_func_ret(paddr_t pc);
 
 void parse_elf(const char *elf_file);
+
 #endif
