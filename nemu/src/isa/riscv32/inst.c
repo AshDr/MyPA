8b48c9ce724eac3df167c0fbc93b3177d624a398
tracer-ics2023 <tracer@njuics.org>
2025-01-14 14:52:44 +0800

    >  compile NEMU 221220000 张三 Linux ashdr 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  14:52:44 up 125 days, 22:11,  1 user,  load average: 0.21, 0.46, 0.35

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -115,6 +115,28 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
   // }
 }
 
+static vaddr_t *csr_register(word_t imm) {
+  switch (imm) {
+  case 0x341:
+    return &(cpu.csrs.mepc);
+  case 0x342:
+    return &(cpu.csrs.mcause);
+  case 0x300:
+    return &(cpu.csrs.mstatus);
+  case 0x305:
+    return &(cpu.csrs.mtvec);
+  default:
+    panic("Unknown csr");
+  }
+}
+
+#define ECALL(dnpc)                                                            \
+  {                                                                            \
+    bool success;                                                              \
+    dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc));           \
+  }
+#define CSR(i) *csr_register(i)
+
 static int decode_exec(Decode *s) {
   int rd = 0;
   word_t src1 = 0, src2 = 0, imm = 0;
@@ -238,7 +260,11 @@ static int decode_exec(Decode *s) {
           R(rd) = src1 / src2);
   INSTPAT("0100000 ????? ????? 101 ????? 01100 11", sra, R,
           R(rd) = (sword_t)src1 >> BITS(src2, 4, 0));
-
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw, I, R(rd) = CSR(imm);
+          CSR(imm) = src1);
+  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs, I, R(rd) = CSR(imm);
+          CSR(imm) |= src1);
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall, I, ECALL(s->dnpc));
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
 
   INSTPAT_END();
