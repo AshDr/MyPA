522fa7fbe150ad77b42f45057056452926ca07e7
tracer-ics2023 <tracer@njuics.org>
2025-01-23 00:39:32 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:39:32 up  2:19,  3 users,  load average: 0.18, 0.19, 0.18

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,28 +17,28 @@
 #include <cpu/difftest.h>
 #include <isa.h>
 
+#define CHECKDIFF_CSRS(p) if (ref_r->csrs.p != cpu.csrs.p) { \
+  printf("CRS: difftest fail at " #p ", expect %#x got %#x\n", ref_r->csrs.p, cpu.csrs.p); \
+  return false; \
+}
+#define CHECKDIFF_FMT(p, fmt, ...) if (ref_r->p != cpu.p) { \
+  printf("difftest fail at " fmt ", expect %#x got %#x\n", ## __VA_ARGS__, ref_r->p, cpu.p); \
+  return false; \
+}
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int reg_num = ARRLEN(cpu.gpr);
   for (int i = 0; i < reg_num; i++) {
-    if (ref_r->gpr[i] != cpu.gpr[i]) {
-      return false;
-    }
+    CHECKDIFF_FMT(gpr[i], "gpr[%d]", i);
   }
   if (ref_r->pc != cpu.pc) {
+    printf("difftest fail at pc, expect %#x got %#x\n", ref_r->pc, cpu.pc);
     return false;
   }
-  if (ref_r->csrs.mcause != cpu.csrs.mcause) {
-    return false;
-  }
-  if (ref_r->csrs.mepc != cpu.csrs.mepc) {
-    return false;
-  }
-  if (ref_r->csrs.mstatus != cpu.csrs.mstatus) {
-    return false;
-  }
-  if (ref_r->csrs.mtvec != cpu.csrs.mtvec) {
-    return false;
-  }
+  CHECKDIFF_CSRS(mstatus);
+  CHECKDIFF_CSRS(mtvec);
+  CHECKDIFF_CSRS(mepc);
+  CHECKDIFF_CSRS(mcause);
   return true;
 }
 
