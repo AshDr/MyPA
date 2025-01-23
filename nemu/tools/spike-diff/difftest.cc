a7f99a4fe1c346aaebec5cc16a878245e4dee520
tracer-ics2023 <tracer@njuics.org>
2025-01-23 13:07:50 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:07:50 up  3:01,  3 users,  load average: 0.04, 0.05, 0.08

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -52,7 +52,7 @@ static state_t *state = NULL;
 void sim_t::diff_init(int port) {
   p = get_core("0");
   state = p->get_state();
-  state->mstatus->write(0x1800);  
+  state->mstatus->unlogged_write(0x1800);  
 }
 
 void sim_t::diff_step(uint64_t n) {
@@ -68,7 +68,7 @@ void sim_t::diff_get_regs(void* diff_context) {
   ctx->mcause = state->mcause->read();
   ctx->mepc = state->mepc->read();
   ctx->mstatus = state->mstatus->read();
-  ctx->mtvec = state->mtvec->read();
+  ctx->mtvec = state->mtvec->read(); // shared_ptr->read()
 }
 
 void sim_t::diff_set_regs(void* diff_context) {
