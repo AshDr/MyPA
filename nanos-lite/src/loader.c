f97ce1fb42eb08ceb0c0e29c83bcdf4921a36c52
tracer-ics2023 <tracer@njuics.org>
2025-01-25 17:34:26 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:34:26 up  2:15,  4 users,  load average: 0.12, 0.11, 0.11

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,7 +29,7 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
-  assert((*(uint32_t *)ehdr.e_ident == 0x7F454C46));
+  assert(((uint32_t) ehdr.e_ident == 0x7F454C46));
   assert(EXPECT_TYPE == ehdr.e_machine);
   uint32_t phnum = ehdr.e_phnum;
   Elf_Phdr phdr;
