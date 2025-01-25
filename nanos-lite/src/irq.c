861b2943e67feb73b2454013d1b2fbef7fa3328a
tracer-ics2023 <tracer@njuics.org>
2025-01-25 22:23:58 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:23:58 up  7:04,  6 users,  load average: 0.24, 0.32, 0.33

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,9 @@
 #include <common.h>
-
+extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: Log("Yield event happened!"); break;
-    case EVENT_SYSCALL: Log("Syscall event happened!"); break;
+    case EVENT_SYSCALL: do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
