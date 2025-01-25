fc2399133fab2872275ee1363f3d57551aac5f4d
tracer-ics2023 <tracer@njuics.org>
2025-01-25 17:44:09 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:44:09 up  2:24,  4 users,  load average: 0.40, 0.23, 0.14

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,38 +27,38 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  // Elf_Ehdr ehdr;
-  // ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  // printf("ident: %x\n", ehdr.e_ident);
-  // assert(((uint32_t)ehdr.e_ident == 0x464c457f));
-  // assert(EXPECT_TYPE == ehdr.e_machine);
-  // uint32_t phnum = ehdr.e_phnum;
-  // Elf_Phdr phdr;
-  // for(int i = 0; i < phnum; i++) {
-  //   ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(Elf_Phdr));
-  //   if(phdr.p_type == PT_LOAD) {
-  //     ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
-  //     if(phdr.p_filesz < phdr.p_memsz) {
-  //       memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
-  //     }
-  //   }
-  // }
-  // return ehdr.e_entry;
-  uint32_t phdr_size = sizeof(Elf_Phdr);
-  Elf_Ehdr *ehdr = malloc(sizeof(Elf_Ehdr));
-  Elf_Phdr *phdr = malloc(phdr_size);
-  ramdisk_read((void*)ehdr, 0, sizeof(Elf_Ehdr));
-  assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
-  printf("ident: %x\n", ehdr->e_ident);
-  assert(EXPECT_TYPE == ehdr->e_machine);
-  uint32_t phdr_num = ehdr->e_phnum;
-  for(int i = 0; i < phdr_num; i++) {
-    ramdisk_read((void*)phdr, ehdr->e_phoff + i * phdr_size, phdr_size);
-    if(phdr->p_type != PT_LOAD) continue;
-    ramdisk_read((void*)phdr->p_vaddr, phdr->p_offset, phdr->p_filesz);
-    memset((void*)phdr->p_vaddr + phdr->p_filesz, 0, phdr->p_memsz - phdr->p_filesz);
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  printf("ident: %x\n", ehdr.e_ident);
+  assert(((uint32_t)ehdr.e_ident == 0x464c457f));
+  assert(EXPECT_TYPE == ehdr.e_machine);
+  uint32_t phnum = ehdr.e_phnum;
+  Elf_Phdr phdr;
+  for(int i = 0; i < phnum; i++) {
+    ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(Elf_Phdr));
+    if(phdr.p_type == PT_LOAD) {
+      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+      if(phdr.p_filesz < phdr.p_memsz) {
+        memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+      }
+    }
   }
-  return ehdr->e_entry;
+  return ehdr.e_entry;
+  // uint32_t phdr_size = sizeof(Elf_Phdr);
+  // Elf_Ehdr *ehdr = malloc(sizeof(Elf_Ehdr));
+  // Elf_Phdr *phdr = malloc(phdr_size);
+  // ramdisk_read((void*)ehdr, 0, sizeof(Elf_Ehdr));
+  // assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
+  // printf("ident: %x\n", ehdr->e_ident);
+  // assert(EXPECT_TYPE == ehdr->e_machine);
+  // uint32_t phdr_num = ehdr->e_phnum;
+  // for(int i = 0; i < phdr_num; i++) {
+  //   ramdisk_read((void*)phdr, ehdr->e_phoff + i * phdr_size, phdr_size);
+  //   if(phdr->p_type != PT_LOAD) continue;
+  //   ramdisk_read((void*)phdr->p_vaddr, phdr->p_offset, phdr->p_filesz);
+  //   memset((void*)phdr->p_vaddr + phdr->p_filesz, 0, phdr->p_memsz - phdr->p_filesz);
+  // }
+  // return ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
