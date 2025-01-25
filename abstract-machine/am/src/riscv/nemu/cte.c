cd23e3099252c640e3c58ef12bd345bda7600372
tracer-ics2023 <tracer@njuics.org>
2025-01-25 22:25:57 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:25:57 up  7:06,  6 users,  load average: 0.27, 0.28, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,11 +13,12 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 11: {
-        if(c->GPR1 == -1) {
-          ev.event = EVENT_YIELD;
-        } else {
-          ev.event = EVENT_SYSCALL;
-        }
+        // if(c->GPR1 == -1) {
+        //   ev.event = EVENT_YIELD;
+        // } else {
+        //   ev.event = EVENT_SYSCALL;
+        // }
+        ev.event = EVENT_YIELD;
         break;
       }
       default: ev.event = EVENT_ERROR; break;
