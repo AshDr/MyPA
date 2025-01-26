c86e759d78660aad6686b870440fc2410b089b08
tracer-ics2023 <tracer@njuics.org>
2025-01-26 20:49:18 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:49:18 up  1:00,  4 users,  load average: 0.99, 0.35, 0.19

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
+#include <NDL.h>
 int main() {
     struct timeval tv;
     struct timezone tz;
     gettimeofday(&tv, &tz);
+    NDL_Init();
+    uint32_t start = NDL_GetTicks();
+    NDL_Quit();
     return 0;
 }
