22b4af6e2e64e90f02e5208743fa3e50f4370b98
tracer-ics2023 <tracer@njuics.org>
2025-01-26 18:47:46 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:47:46 up  4:20,  4 users,  load average: 0.11, 0.22, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -70,18 +70,21 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 size_t fs_lseek(int fd, size_t offset, int whence) {
   switch(whence) {
     case SEEK_SET: {
-      assert(offset >= file_table[fd].disk_offset && offset <= file_table[fd].disk_offset + file_table[fd].size);
       file_table[fd].open_offset = offset; break;
     }
     case SEEK_CUR: {
-      assert(file_table[fd].open_offset + offset >= file_table[fd].disk_offset && file_table[fd].open_offset + offset <= file_table[fd].disk_offset + file_table[fd].size);
       file_table[fd].open_offset += offset; break;
     }
     case SEEK_END: {
-      assert(file_table[fd].size + offset >= file_table[fd].disk_offset && file_table[fd].size + offset <= file_table[fd].disk_offset + file_table[fd].size);
       file_table[fd].open_offset = file_table[fd].size + offset; break;
     }
     default: panic("invalid whence %d", whence);
+  }
+  if(file_table[fd].open_offset > file_table[fd].size) {
+    file_table[fd].open_offset = file_table[fd].size;
+  }
+  if(file_table[fd].open_offset < 0) {
+    file_table[fd].open_offset = 0;
   }
   return file_table[fd].open_offset;
 }
