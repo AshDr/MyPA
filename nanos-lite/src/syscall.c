00ce3c1d3783a582806f4de0e771cba1a82edbeb
tracer-ics2023 <tracer@njuics.org>
2025-01-26 14:56:26 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:56:26 up 28 min,  3 users,  load average: 0.08, 0.06, 0.04

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,16 +3,37 @@
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
-
+  a[0] = c->GPR1; // a7
+  a[1] = c->GPR2; // a0
+  a[2] = c->GPR3; // a1
+  a[3] = c->GPR4; // a2
   switch (a[0]) {
     case SYS_exit: {
-      halt(0);
+      #ifdef CONFIG_STRACE
+      Log("Syscall: SYS_exit");
+      #endif
+      halt(0); // need change ?
       break;
     }
     case SYS_yield: {
+      #ifdef CONFIG_STRACE
+      Log("Syscall: SYS_yield");
+      #endif
       yield();
       c->GPRx = 0; 
+      break;
+    }
+    case SYS_write: {
+      #ifdef CONFIG_STRACE
+      Log("Syscall: SYS_write");
+      #endif
+      // int fd = a[1];
+      const void *buf = (const void *)a[2];
+      size_t count = a[3];
+      for(size_t i = 0; i < count; i++) {
+        putch(((char *)buf)[i]);
+      }
+      c->GPRx = count;
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
