8b4db1cbd93e69c35931707a2adde4f0fadd682b
tracer-ics2023 <tracer@njuics.org>
2025-01-26 15:18:07 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:18:07 up 50 min,  4 users,  load average: 0.56, 0.29, 0.26

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -67,7 +67,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   assert(fd == 1 || fd == 2);
-  printf("_write: fd=%d, buf=%s, count=%d\n", fd, buf, count);
+  // printf("_write: fd=%d, buf=%s, count=%d\n", fd, buf, count);
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
