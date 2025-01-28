263173dd01eeee08061da44cfd6989dfa58f5ac4
tracer-ics2023 <tracer@njuics.org>
2025-01-28 17:33:29 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:33:29 up  2:21,  4 users,  load average: 0.38, 0.26, 0.26

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -21,12 +21,12 @@ int SDL_WaitEvent(SDL_Event *event) {
   char buf[20];
   while (1) {
     if (NDL_PollEvent(buf, sizeof(buf)) == 0) continue;
-    printf("%s?\n",buf);
     event->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
+    printf("buf:%s",buf);
+    printf("len=%d\n",strlen(buf));
     for (int i = 0; i < 83; i++) {
       if (strcmp(keyname[i], buf + 3) == 0) {
         event->key.keysym.sym = i;
-        printf("return %d\n", i);
         return 1;
       }
     }
