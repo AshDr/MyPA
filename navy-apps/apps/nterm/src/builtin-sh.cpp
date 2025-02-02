646ef4b0435ba2cd93a5c3243d50d79b42c19767
tracer-ics2023 <tracer@njuics.org>
2025-02-02 16:30:55 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-6ECP7PUB 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:30:55 up  4:20,  4 users,  load average: 0.40, 0.39, 0.32

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -28,8 +28,8 @@ static void sh_handle_cmd(const char *cmd) {
   memcpy(str, cmd, sizeof(char) * len);
   str[len - 1] = 0;
   // execve(str, NULL, NULL);
-  sh_printf("terminal execvep: %s\n", str);
-  for(volatile int i = 0; i < 10000000; i++);
+  printf("terminal execvep: %s\n", str);
+  sleep(2);
   execvp(str, NULL);
 }
 
